a0ef22731a56e09108e595f7b057a0f98f7fd5b3
박진영 <34739974+nazzang49@users.noreply.github.com>
2022-10-14 18:42:19 +0900

    Update dag_test.py

--- dags/dag_test.py
+++ dags/dag_test.py
@@ -29,7 +29,7 @@ default_args = {
 }
 
 with DAG('databricks_dag',
-    start_date=datetime(2022, 10, 25),
+    start_date=datetime(2022, 10, 23),
     schedule_interval='@daily',
     catchup=False,
     default_args=default_args
