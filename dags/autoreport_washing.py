ecac7a5a6ede184610b254ed6a25413117c6dcd0
PTBWA\jinyoung.park <jinyoung.park@ptbwa.com>
2022-11-25 19:32:45 +0900

    check run now

--- dags/autoreport_washing.py
+++ dags/autoreport_washing.py
@@ -12,23 +12,22 @@ def create_notebook_params(**context):
     notebook_params = dict()
     notebook_names = list()
     base_path = "/usr/local/airflow/dags" # based on MWAA
-    # for config_file in os.listdir(base_path):
+    for config_file in os.listdir(base_path):
+        config = config_file.split(".")
 
-    context["task_instance"].xcom_push(key="path", value=os.listdir(base_path))
+        if len(config) <= 1:
+            continue
 
-    #     print(os.listdir(base_path))
-    #     print(config_file)
-    #
-    #     notebook_name, file_ext = config_file.split(".")
-    #     if file_ext == "json":
-    #         notebook_names.append(notebook_name)
-    #         with open(os.path.join(base_path, config_file), "r", encoding="utf-8") as f:
-    #             notebook_params[notebook_name] = json.dumps(json.load(f))
-    # notebook_params["notebook_names"] = ",".join(notebook_names)
-    # return
+        if config[1] == "json":
+            notebook_names.append(config[0])
+            with open(os.path.join(base_path, config_file), "r", encoding="utf-8") as f:
+                notebook_params[config[0]] = json.dumps(json.load(f))
 
-def check_xcom_pull(**context):
-    context["task_instance"].xcom_pull(task_ids="create_notebook_params_task", key="path")
+    notebook_params["notebook_names"] = ",".join(notebook_names)
+    context["task_instance"].xcom_push(key="notebook_params", value=notebook_params)
+
+# def check_xcom_pull(**context):
+#     context["task_instance"].xcom_pull(task_ids="create_notebook_params_task", key="path")
 
 default_args = {
     'owner': 'airflow',
@@ -51,20 +50,20 @@ with DAG('autoreport_washing',
         python_callable=create_notebook_params
     )
 
-    check_xcom_pull_run = PythonOperator(
-        task_id="check_xcom_pull_run_task",
-        python_callable=check_xcom_pull
-    )
-
-    # washing_run = DatabricksRunNowOperator(
-    #     task_id="washing_task",
-    #     job_id="751730826324009",
-    #     databricks_conn_id='databricks_default',
-    #     notebook_params='{{ task_instance.xcom_pull(task_ids="create_notebook_params_task") }}'
+    # check_xcom_pull_run = PythonOperator(
+    #     task_id="check_xcom_pull_run_task",
+    #     python_callable=check_xcom_pull
     # )
+
+    washing_run = DatabricksRunNowOperator(
+        task_id="washing_task",
+        job_id="751730826324009",
+        databricks_conn_id='databricks_default',
+        notebook_params='{{ task_instance.xcom_pull(task_ids="create_notebook_params_task", key="notebook_params") }}'
+    )
 
     start_run = DummyOperator(task_id="start")
     end_run = DummyOperator(task_id="end")
 
-    # start_run >> create_notebook_params_run >> washing_run >> end_run
-    start_run >> create_notebook_params_run >> check_xcom_pull_run >> end_run
+    start_run >> create_notebook_params_run >> washing_run >> end_run
+    # start_run >> create_notebook_params_run >> check_xcom_pull_run >> end_run
