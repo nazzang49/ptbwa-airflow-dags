21dae06f064d4b4b2f9f762092665a358567958d
PTBWA\jinyoung.park <jinyoung.park@ptbwa.com>
2023-01-17 15:53:33 +0900

    update dags : change kcar schedule_interval for test

--- dags/autoreport_daily_kcar.py
+++ dags/autoreport_daily_kcar.py
@@ -118,7 +118,7 @@ default_args = {
 }
 with DAG('autoreport_kcar_dag',
     start_date=datetime(2022, 12, 19, tzinfo=Timezone("Asia/Seoul")),
-    schedule_interval=None,
+    schedule_interval="0 16 * * *",
     catchup=False,
     default_args=default_args,
     render_template_as_native_obj=True,
