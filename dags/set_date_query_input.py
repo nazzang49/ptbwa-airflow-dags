2f33790204e98134bd2fe94e259e0c5ee51a2c0c
h-y-e-j-i <11th_hj@naver.com>
2023-02-27 10:42:19 +0900

    rollback

--- dags/set_date_query_input.py
+++ dags/set_date_query_input.py
@@ -1,67 +1,82 @@
-import airflow.utils.timezone
-
 from airflow import DAG
-from airflow.models import Variable, DagModel
 from airflow.operators.python import PythonOperator
 from airflow.operators.dummy import DummyOperator
-from airflow.models import DagModel
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.utils.trigger_rule import TriggerRule
+from airflow.models import Variable, DagModel
 
-from datetime import timedelta
+from datetime import timedelta, datetime
+from dateutil.relativedelta import relativedelta
+from pendulum.tz.timezone import Timezone
 
-from user_function import unpause_dag, pause_dag
+from user_function import * 
 
+ 
 default_args={
     "owner" : "hyeji",
     "provide_context" : True,
     "depends_on_past" : False,
-    "start_date" : airflow.utils.timezone.datetime(2023, 1, 18),
-    "retries" : 5,
-    "retry_delay" : timedelta(minutes=5),
+    # "retries" : 1
+    # "start_date" : airflow.utils.timezone.datetime(2023, 2, 15),
 }
 
 with DAG(
-     dag_id = "tt-delete_variable",
+    dag_id = "tt-set_date_query_input",
+    # start_date = airflow.utils.timezone.datetime(2023, 1, 18),
     default_args = default_args,
-    schedule_interval = None
-) as dag:
+    # schedule_interval = "0 1 * * *",
+    schedule_interval = None,
+    # schedule_interval = "@daily",
+    start_date = datetime(2023, 2, 23, tzinfo=Timezone("Asia/Seoul")),
+    catchup = False
+    
+) as dag: 
 
-    def _delete_variable(variable_key):
-        for k in variable_key:
-            Variable.delete(key=k)
-
-
-    before_dag = DummyOperator(
-        task_id = "tt-crawling_info_dag"
-    )
-
-    delete_variable = PythonOperator(
-        task_id = "tt-delete_variable",
-        python_callable = _delete_variable,
+    
+    tt_set_input_specific_task = PythonOperator(
+        task_id = "tt-set_input_specific_task",
+        python_callable = set_crawling_date_query,
         op_kwargs = {
-            "variable_key" : ["bundle_list", "bundle_len", "query", "crawling_date", "crawling_since_date", "crawling_until_date"]
+            "crawling_date" : "{{dag_run.conf.crawling_date}}",
+            "crawling_since_date" : "{{dag_run.conf.crawling_since_date}}",
+            "crawling_until_date" : "{{dag_run.conf.crawling_until_date}}",
         }
     )
 
-    pause_crawling_info_dag = PythonOperator(
-        task_id = "tt-pause_crawling_info_dag",
-        python_callable = pause_dag,
+    tt_set_input_yesterday_task = PythonOperator(
+        task_id = "tt-set_input_yesterday_task",
+        python_callable = set_crawling_date_query,
         op_kwargs = {
-            "dag_id" :"tt-crawling_info"
-        }
+            "crawling_date" : "{{dag_run.conf.crawling_date}}",
+            "crawling_since_date" : None,
+            "crawling_until_date" : None,
+        },
+        trigger_rule = TriggerRule.ALL_FAILED
     )
-
-    pause_delete_variable_dag = PythonOperator(
-        task_id = "tt-pause_delete_variable_dag",
-        python_callable = pause_dag,
+    
+    unpause_get_bundle_dag = PythonOperator(
+        task_id = "tt-unpause_get_bundle_dag",
+        python_callable = unpause_dag,
         op_kwargs = {
-            "dag_id" :"tt-delete_variable"
-        }
+            "dag_id" :"tt-get_bundle"
+        },
+        trigger_rule = TriggerRule.ALL_DONE
     )
 
-    end = DummyOperator(
-        task_id = "end"
+    trigger_crawling_info_dag = TriggerDagRunOperator(
+        task_id = "tt-trigger_get_bundle_dag",
+        trigger_dag_id = "tt-get_bundle",
+        # trigger_rule = TriggerRule.ALL_DONE  
     )
 
-    before_dag>> pause_crawling_info_dag >> delete_variable >> pause_delete_variable_dag >> end
+    # next_dag = DummyOperator(
+    #     task_id = "tt-get_bundle_dag_dag",
+    #     trigger_rule = TriggerRule.ALL_DONE
+    # )
+
+    tt_set_input_specific_task >> tt_set_input_yesterday_task >> unpause_get_bundle_dag >> trigger_crawling_info_dag #>> next_dag
+
+
+
 
 
